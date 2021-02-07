b738ed0938c26de73af96b15ee8ba614eb1d4533
MichaelTA <michaelallen0111@gmail.com>
2021-02-07 03:04:38 +0000

    Added exporting data as a csv

---
+++ Desktop Application Source/Exporter.py
@@ -0,0 +1,42 @@
+from TimeConverter import TimeConverter as Tc
+
+
+utds = Tc.unix_to_date_string
+
+
+class Exporter:
+    """
+    Static class to export data as a csv
+    """
+    @staticmethod
+    def export(headings, data, file_path, unix=False):
+        """
+        Static method to export the data as a csv
+        :param headings: The headings that were queried in the database
+        :param data: The data as an array
+        :param file_path: The file path to store the csv
+        :param unix: Boolean to save the time as a unix timestamp or a date and time
+        """
+        if unix:
+            headings = 'timestamp,' + headings.replace(' ', '')
+        else:
+            headings = 'date_and_time,' + headings.replace(' ', '')
+            for i in range(len(data)):
+                data[i] = list(data[i])
+                data[i][0] = utds(data[i][0])
+
+        try:
+            f = open(file_path, 'x')  # Create the csv file
+            f.close()
+
+            f = open(file_path, 'a')  # Write to the csv file
+            f.write(headings)
+            for i in range(len(data)):
+                f.write('\n' + ','.join(map(str, data[i])))
+            f.close()
+        except FileExistsError:
+            print('File already exists!')
+
+
+if __name__ == '__main__':
+    print('Please run a different source file.')
--- Desktop Application Source/run.py
+++ Desktop Application Source/run.py
@@ -1,6 +1,7 @@
 from DatabaseManager import db_read
 from TimeConverter import TimeConverter as Tc
 from StatisticsManager import StatisticsManager as Sm
+from Exporter import Exporter as Ex
 
 
 dtu = Tc.date_to_unix
@@ -14,10 +15,15 @@ if __name__ == '__main__':
     headings = ['temperature',
                 'carbon_monoxide, nitric_oxide, nitrogen_dioxide, sulphur_dioxide',
                 'carbon_monoxide, sulphur_dioxide']
+    file_paths = ['test1.csv', 'test2.csv', 'test3.csv']
     stats = []
 
-    for i in range(3):
+    for i in range(len(times)):
         raw, time, data = db_read(times[i], headings[i], plot_graph=True)
+        if i == 1:
+            Ex.export(headings[i], raw, file_paths[i], unix=True)
+        else:
+            Ex.export(headings[i], raw, file_paths[i], unix=False)
         stats.append([])
         for j in range(len(data)):
             stats[i].append(Sm(data[j], time))
