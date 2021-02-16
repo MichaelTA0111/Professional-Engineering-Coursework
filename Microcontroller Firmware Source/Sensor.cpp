2874a31e6d536038dbbb833d96e2eef36c28865c
MichaelTA <michaelallen0111@gmail.com>
2021-02-16 01:13:31 +0000

    Redone comments

--- Microcontroller Firmware Source/Sensor.cpp
+++ Microcontroller Firmware Source/Sensor.cpp
@@ -4,49 +4,35 @@
 
 #include "Sensor.h"
 
-/**
- * Constructor to create a sensor object with a negative median reading.
- */
 Sensor::Sensor() {
     medianReading = -300;
 }
 
-/**
- * Getter for the median reading
- * @return - The median reading
- */
 double Sensor::getMedianReading() const {
     return medianReading;
 }
 
-/**
- * Method to add a new reading to the vector of readings
- * @param reading - The reading to be appended to the vector
- */
 void Sensor::newReading(double reading) {
     readings.push_back(reading);
 }
 
-/**
- * Method to calculate the median value from the vector of readings
- */
 void Sensor::calculateMedian() {
     if (!readings.empty()) {
         int length = (int) readings.size();
         if (readings.size() % 2 == 0) {
+            // Median of an even number of total readings
             medianReading = (readings[length / 2 - 1] + readings[length / 2]) / 2;
         } else {
+            // Median of an odd number of total readings
             medianReading = readings[length / 2];
         }
     } else {
+        // Default value of -300 is out of range for all sensors, signifies no reading was taken
         medianReading = -300;
     }
     resetReadings();
 }
 
-/**
- * Private method to remove all readings from the vector of previous readings
- */
 void Sensor::resetReadings() {
     readings.clear();
 }
--- Microcontroller Firmware Source/Sensor.h
+++ Microcontroller Firmware Source/Sensor.h
@@ -5,6 +5,7 @@
 #ifndef MBED_SENSOR_H
 #define MBED_SENSOR_H
 
+
 #include <vector>
 
 /**
@@ -13,15 +14,36 @@
  */
 class Sensor {
     public:
+        /**
+        * Constructor to create a sensor object with a negative median reading.
+        */
         Sensor();
+
+        /**
+         * Getter for the median reading
+         * @return - The median reading
+         */
         [[nodiscard]] double getMedianReading() const;
+
+        /**
+         * Method to add a new reading to the vector of readings
+         * @param reading - The reading to be appended to the vector
+         */
         virtual void newReading(double reading);
+
+        /**
+         * Method to calculate the median value from the vector of readings
+         */
         void calculateMedian();
 
     private:
+        /**
+         * Private method to remove all readings from the vector of previous readings
+         */
+        void resetReadings();
+
         double medianReading;
         std::vector<double> readings;
-        void resetReadings();
 };
 
 #endif //MBED_SENSOR_H
