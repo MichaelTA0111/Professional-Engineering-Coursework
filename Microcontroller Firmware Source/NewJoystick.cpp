fd8f8ef5d39ba1422e4d15d1b79c0098fe7df1e4
David Lim <37480082+drlim2u@users.noreply.github.com>
2021-04-13 08:01:50 +0100

    Create NewJoystick

---
+++ Microcontroller Firmware Source/NewJoystick.cpp
@@ -0,0 +1,27 @@
+//
+// Created by drlim on 13/04/2021.
+//
+
+#include "NewJoystick.h"
+
+Joystick::Joystick(const PinName& x_pin, const PinName& y_pin, std::chrono::milliseconds debounce_delay,
+        float low_limit_x, float high_limit_x, float low_limit_y, float high_limit_y)
+        :x_pin(x_pin), y_pin(y_pin), left_button(debounce_delay), right_button(debounce_delay),
+         up_button(debounce_delay), down_button(debounce_delay), low_limit_x(low_limit_x), high_limit_x(high_limit_x),
+         low_limit_y(low_limit_y), high_limit_y(high_limit_y) { }
+
+Direction Joystick::direction_pressed(std::chrono::milliseconds runtime)
+{
+    if (left_button.isPressed(x_pin.read() < low_limit_x, runtime)) {
+        return left;
+    }
+    if (right_button.isPressed(x_pin.read() > high_limit_x, runtime)) {
+        return right;
+    }
+    if (up_button.isPressed(y_pin.read() < low_limit_y, runtime)) {
+        return up;
+    }
+    if (down_button.isPressed(y_pin.read() > high_limit_y, runtime)) {
+        return down;
+    }
+}
---
+++ Microcontroller Firmware Source/NewJoystick.h
@@ -0,0 +1,36 @@
+//
+// Created by drlim on 13/04/2021.
+//
+
+#ifndef PROFESSIONAL_ENGINEERING_COURSEWORK_JOYSTICK_H
+#define PROFESSIONAL_ENGINEERING_COURSEWORK_JOYSTICK_H
+
+#include "mbed.h"
+#include "Button.h"
+#include <chrono>
+
+enum Direction { none = 0, left = 1, right = 2, up = 3, down = 4 };
+
+class Joystick {
+public:
+    Joystick(const PinName& x_pin, const PinName& y_pin, std::chrono::milliseconds debounce_delay,
+            float low_limit_x, float high_limit_x, float low_limit_y, float high_limit_y);
+
+    Direction direction_pressed(std::chrono::milliseconds runtime);
+
+private:
+    AnalogIn x_pin;
+    AnalogIn y_pin;
+
+    Button left_button;
+    Button right_button;
+    Button up_button;
+    Button down_button;
+
+    float low_limit_x;
+    float high_limit_x;
+    float low_limit_y;
+    float high_limit_y;
+};
+
+#endif //PROFESSIONAL_ENGINEERING_COURSEWORK_JOYSTICK_H
